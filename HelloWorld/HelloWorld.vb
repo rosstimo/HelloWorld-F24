0ccefdb72068bfbad6eb6d24fe63d7519321b2da
rosstimo <rosstimo@isu.edu>
2024-08-20 08:41:48 -0600

    add header

--- HelloWorld/HelloWorld.vb
+++ HelloWorld/HelloWorld.vb
@@ -1,4 +1,10 @@
-﻿Module HelloWorld
+﻿'Tim Rossiter
+'RCET2265
+'Fall 2024
+'Hello, World Program
+'https://github.com/rosstimo/HelloWorld-F24.git
+
+Module HelloWorld
 
     Sub Main()
         Console.WriteLine("Hello, World!")
